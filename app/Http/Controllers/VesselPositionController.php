7eda102e2c0a5b94c56704d3488200bd6befd9ab
c4turla <catur.crh@gmail.com>
2026-06-09 01:36:11 +0700

    feat: improve vessel position tracking logic on map based on departure schedules

--- app/Http/Controllers/VesselPositionController.php
+++ app/Http/Controllers/VesselPositionController.php
@@ -30,63 +30,96 @@ class VesselPositionController extends Controller
 
         foreach ($vessels as $vessel) {
             $lastArrival = $vessel->arrivals->first();
+            $lastDeparture = $vessel->departures->first();
             
-            // If the vessel has never arrived, we don't know where it is
-            if (!$lastArrival || !$lastArrival->landing_site_id) {
+            if (!$lastArrival && !$lastDeparture) {
                 continue;
             }
             
-            $lastDeparture = $vessel->departures->first();
-            $isAtPort = true;
-
-            // Formatting correctly to merge date and time strings for comparison
-            $arrDateStr = $lastArrival->arrival_date instanceof Carbon 
-                ? $lastArrival->arrival_date->format('Y-m-d') 
-                : Carbon::parse($lastArrival->arrival_date)->format('Y-m-d');
-
-                // Use format('H:i:s') to get only the time part from the casted arrival_time Carbon object
+            // Get exact timestamps for comparison
+            $arrTime = null;
+            if ($lastArrival) {
+                $arrDateStr = $lastArrival->arrival_date instanceof Carbon 
+                    ? $lastArrival->arrival_date->format('Y-m-d') 
+                    : Carbon::parse($lastArrival->arrival_date)->format('Y-m-d');
                 $arrTime = Carbon::parse($arrDateStr . ' ' . ($lastArrival->arrival_time instanceof Carbon ? $lastArrival->arrival_time->format('H:i:s') : $lastArrival->arrival_time));
-                
-                if ($lastDeparture) {
+            }
+
+            $depTime = null;
+            if ($lastDeparture) {
+                $depTime = $lastDeparture->departure_datetime 
+                    ? Carbon::parse($lastDeparture->departure_datetime) 
+                    : null;
+                    
+                if (!$depTime) {
                     $depDateStr = $lastDeparture->departure_date instanceof Carbon 
                         ? $lastDeparture->departure_date->format('Y-m-d') 
                         : Carbon::parse($lastDeparture->departure_date)->format('Y-m-d');
-
                     $depTime = Carbon::parse($depDateStr . ' ' . ($lastDeparture->departure_time instanceof Carbon ? $lastDeparture->departure_time->format('H:i:s') : $lastDeparture->departure_time));
-
-                    // If the most recent departure happened strictly after the most recent arrival,
-                    // the vessel is at sea.
-                    if ($depTime->gt($arrTime)) {
-                        $isAtPort = false;
-                    }
                 }
+            }
 
-                if ($isAtPort) {
-                    $siteId = $lastArrival->landing_site_id;
-                    $site = $landingSites->get($siteId);
-                    
-                    // Only consider landing sites that have coordinate data
-                    if ($site && $site->latitude && $site->longitude) {
-                        if (!isset($positions[$siteId])) {
-                            $positions[$siteId] = [
-                                'id' => $site->id,
-                                'name' => $site->site_name,
-                                'latitude' => $site->latitude,
-                                'longitude' => $site->longitude,
-                                'vessels' => []
-                            ];
-                        }
-                        
-                        $positions[$siteId]['vessels'][] = [
-                            'id' => $vessel->id,
-                            'name' => $vessel->vessel_name,
-                            'owner' => $vessel->owner_name ?? '-',
-                            'gt' => $vessel->gt ?? '-',
-                            'status' => $lastArrival->status,
-                            'arrival_time' => $arrTime->diffForHumans()
+            $isAtPort = false;
+            $currentSiteId = null;
+            $statusStr = '';
+            $timeRef = null;
+
+            if ($lastArrival && $lastDeparture) {
+                // Compare actual event times
+                if ($depTime->gt($arrTime)) {
+                    // Departure is the most recent event
+                    if (now()->lte($depTime)) {
+                        $isAtPort = true;
+                        $currentSiteId = $lastDeparture->landing_site_id;
+                        $statusStr = 'Persiapan Berangkat';
+                        $timeRef = $depTime;
+                    }
+                } else {
+                    // Arrival is the most recent event
+                    $isAtPort = true;
+                    $currentSiteId = $lastArrival->landing_site_id;
+                    $statusStr = $lastArrival->status;
+                    $timeRef = $arrTime;
+                }
+            } elseif ($lastArrival) {
+                $isAtPort = true;
+                $currentSiteId = $lastArrival->landing_site_id;
+                $statusStr = $lastArrival->status;
+                $timeRef = $arrTime;
+            } elseif ($lastDeparture) {
+                if (now()->lte($depTime)) {
+                    $isAtPort = true;
+                    $currentSiteId = $lastDeparture->landing_site_id;
+                    $statusStr = 'Persiapan Berangkat';
+                    $timeRef = $depTime;
+                }
+            }
+
+            if ($isAtPort && $currentSiteId) {
+                $site = $landingSites->get($currentSiteId);
+                
+                // Only consider landing sites that have coordinate data
+                if ($site && $site->latitude && $site->longitude) {
+                    if (!isset($positions[$currentSiteId])) {
+                        $positions[$currentSiteId] = [
+                            'id' => $site->id,
+                            'name' => $site->site_name,
+                            'latitude' => $site->latitude,
+                            'longitude' => $site->longitude,
+                            'vessels' => []
                         ];
                     }
+                    
+                    $positions[$currentSiteId]['vessels'][] = [
+                        'id' => $vessel->id,
+                        'name' => $vessel->vessel_name,
+                        'owner' => $vessel->owner_name ?? '-',
+                        'gt' => $vessel->gt ?? '-',
+                        'status' => $statusStr,
+                        'arrival_time' => $timeRef ? $timeRef->diffForHumans() : '-'
+                    ];
                 }
+            }
         }
 
         return Inertia::render('VesselPositions/Index', [
